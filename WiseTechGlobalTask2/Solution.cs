35840b48059767d852b16cb5b9409eb71a493594
ttaher <talat.t.mohammed@gmail.com>
2018-12-08 12:37:28 +0300

    Add project files.

---
+++ WiseTechGlobalTask2/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace WiseTechGlobalTask2
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int x = solution(new int[] { 2, 1, 3, 5, 4 });
+            Console.WriteLine(x);
+            int x1 = solution(new int[] { 2, 3, 4, 1, 5 });
+            Console.WriteLine(x1);
+            int x2 = solution(new int[] { 1, 3, 4, 2, 5 });
+            Console.WriteLine(x2);
+
+        }
+        public static int solution(int[]a)
+        {
+            
+            int res = 0;
+            var sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+                res++;
+                if (sum >= a.Length)
+                {
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
